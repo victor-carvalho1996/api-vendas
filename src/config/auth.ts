f14c9719a6e7f3f93139fc09c2c159e8e116d0da
victor-carvalho1996 <victorpcarvalho18@gmail.com>
2021-07-02 21:43:55 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '85db6086642ff309cbf75ba2ffce15e3',
+    expireIn: '1d',
+  },
+};
---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,23 @@
+import CreateUserService from '@modules/users/services/CreateUserService';
+import ListUserService from '@modules/users/services/ListUserService';
+import { Request, Response } from 'express';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    return response.json(user);
+  }
+}
